a9e99f2373d2eb80f8343c1688404d7770ca54f5
alexandrechupin <alexandrechupin@gmail.com>
2019-08-30 14:37:00 +0200

    update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import "bootstrap";
 import { initThemechoices } from "../plugins/theme_choices";
 import {showHide} from "../plugins/show_hide";
+import "../plugins/flatpickr"
 
 initThemechoices();
 showHide();
